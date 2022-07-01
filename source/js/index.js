623f5dad460a62b6bc9b14b60022d99ed4c3aead
FantasticFox91 <44309782+FantasticFox91@users.noreply.github.com>
2022-07-01 17:36:33 +0300

    asd

--- source/js/index.js
+++
@@ -1,8 +0,0 @@
-const burgerButton = document.querySelector('.main-nav__toggle');
-burgerButton.classList.remove('main-nav__toggle--noJS');
-burgerButton.classList.add('main-nav__toggle--close');
-
-burgerButton.addEventListener('click', () => {
-  burgerButton.classList.toggle('main-nav__toggle--close')
-  burgerButton.classList.toggle('main-nav__toggle--open');
-})
