a11cb5aad407333931869cece0e9d9750f6ba073
FantasticFox91 <44309782+FantasticFox91@users.noreply.github.com>
2022-07-01 17:34:51 +0300

    finishing project

---
+++ source/js/swiper.js
@@ -0,0 +1,18 @@
+import Swiper from '../../swiper/swiper-bundle.esm.browser.js';
+
+const swiper = new Swiper('.swiper', {
+  // Optional parameters
+  direction: 'horizontal',
+  loop: true,
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
