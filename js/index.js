277fc8dcf93fada32289d1ace471c23de537d75b
FantasticFox91 <44309782+FantasticFox91@users.noreply.github.com>
2022-07-03 11:34:31 +0000

    Deploying to gh-pages from @ FantasticFox91/grading@a58ad2280d551681e0528ece2154c921644c0277 🚀

--- js/index.js
+++ js/index.js
@@ -1,1 +1,1 @@
-import{loadMap}from"./map.js";import{swiper}from"./swiper.js";import{setEventListener}from"./burger-button.js";setEventListener(),swiper.init(),loadMap();
+import{loadMap}from"./map.js";import{swiper}from"./swiper.js";import{setEventListener}from"./burger-button.js";import"./range.js";setEventListener(),swiper.init(),loadMap();
